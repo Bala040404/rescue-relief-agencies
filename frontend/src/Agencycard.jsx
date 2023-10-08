e49e431a3265c3796a6d0fab05702378860c03bf
Dennis Andrew <dennisandrew0805@gmail.com>
2023-10-08 12:29:01 +0530

    changed card display

--- frontend/src/Agencycard.jsx
+++ frontend/src/Agencycard.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import "./Agencycard.css"
+import "./Agencycard.css";
 
 function Agencycard(props) {
-
-
-    let redirect = useNavigate();
-
-
-
-    return (
-
-        <div className={props.cls} onClick={() => redirect(`/${props.agency._id}`)} >
-            <h1>{props.agency.username}</h1>
-            <h2>Expertise - {props.agency.expertise}</h2>
-            <h3>Location - {props.agency.location}</h3>
-            <h3>Contact - {props.agency.contact}</h3>
-        </div >
-    )
+  let redirect = useNavigate();
+  return (
+    <div className={props.cls} onClick={() => redirect(`/${props.agency._id}`)}>
+      <h1>{props.agency.username}</h1>
+      <div className="card-text">
+        <span>
+          <h3>Expertise</h3>
+          <h3>Location</h3>
+          <h3>Contact</h3>
+        </span>
+        <span>
+          <h3>: {props.agency.expertise}</h3>
+          <h3>: {props.agency.location}</h3>
+          <h3>: {props.agency.contact}</h3>
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default Agencycard
+export default Agencycard;
